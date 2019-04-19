2cc52e62b1b0366fc4479afb1f56976020c7e208
Gabrielle Sinacola <gabriellesinacola@Gabrielles-MacBook-Pro-2.local>
2019-04-19 16:17:13 -0700

    first test threading

--- CSCI201-Trojan-Tumble/WebContent/game.jsp
+++ CSCI201-Trojan-Tumble/WebContent/game.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" import="java.io.IOException" import="java.sql.Connection" import="java.sql.DriverManager"
+	import="java.sql.PreparedStatement" import="java.sql.ResultSet" import="java.sql.SQLException"%>
 <!DOCTYPE html>
 <html lang="en"> 
 <head> 
@@ -14,7 +15,48 @@
 </head>
 <%
 	int avatar = (int)session.getAttribute("avatar");
-	//int avatar = 1;
+	String loggedIn = (String)session.getAttribute("loggedIn");
+	String user = "GUEST";
+	if(loggedIn.equals("true")){
+		user = (String)session.getAttribute("user"); 
+		user = user.toUpperCase();
+	}
+	int currentID = 0;
+	Connection conn = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+	
+	String jdbcUrl = "jdbc:mysql://aagurobfnidxze.cesazkri7ef1.us-east-2.rds.amazonaws.com:3306/game?user=user&password=password";					
+	
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver loaded");
+		conn = DriverManager.getConnection(jdbcUrl);
+
+		ps = conn.prepareStatement("INSERT INTO Threads(username) VALUES(?)");
+		ps.setString(1, user);
+		ps.execute();
+		
+		ps = conn.prepareStatement("SELECT COUNT(threadID) FROM Threads");
+		rs = ps.executeQuery();
+		
+		while(rs.next()){
+			currentID = rs.getInt("COUNT(threadID)");
+		}
+		
+	}catch(SQLException sqle) {
+		System.out.println("sqle game: " + sqle.getMessage());
+	}catch(ClassNotFoundException cnfe) {
+		System.out.println("cnfe game: " + cnfe.getMessage());
+	}finally {
+		try {
+			if(rs != null) rs.close();
+			if(ps != null) ps.close();
+			if(conn != null) conn.close();
+		}catch (SQLException sqle) {
+			System.out.println("sqle game finally: " + sqle.getMessage());
+		}
+	}
 %>
 <body>
 
@@ -38,6 +80,10 @@
 		        update: update
 		    }
 		};
+		
+		var currentID = <%=currentID%>;
+		console.log("currentID: " + currentID);
+		var lastOne;
 		
 		var game = new Phaser.Game(config);
 		
@@ -269,6 +315,15 @@
 		/* ---------- Update Game Status Every Frame ----------*/
 		function update ()
 		{	
+			ajaxGet(‘multiServlet?currentID=’+currentID,function(results){
+                if (results != lastOne && results != null && results != “null”){
+                    console.log(“results: “+ results);
+                    console.log(“lastone: “+ lastOne);
+                    lastOne = results;
+                    currentID++;
+
+                }
+            })
 			//start game
 			if(start_count_ready == 120 && start_count_go <= 180){
 				startText.setText('Go!');
@@ -400,6 +455,23 @@
 		    coinCount += 1;
 		    coinText.setText('Coins: ' + coinCount);
 		}
+		
+		function ajaxGet(endpointUrl, returnFunction){
+            var xhr = new XMLHttpRequest();
+            xhr.open(‘GET’, endpointUrl, true);
+            xhr.onreadystatechange = function(){
+                if (xhr.readyState == XMLHttpRequest.DONE) {
+                    if (xhr.status == 200) {
+                        returnFunction( this.responseText );
+
+                    } else {
+                        alert(‘AJAX Error.’);
+                        console.log(xhr.status);
+                    }
+                }
+            }
+            xhr.send();
+        };
 		
 		function collectDiamond (player, diamond)
 		{
---
+++ CSCI201-Trojan-Tumble/src/tumble/multiServlet.java
@@ -0,0 +1,105 @@
+package tumble;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+* Servlet implementation class servlet
+*/
+@WebServlet("/multiServlet")
+public class multiServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   public multiServlet() {
+       super();
+       // TODO Auto-generated constructor stub
+   }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        
+        
+        String currentID = request.getParameter("currentID");
+        String start = request.getParameter("start");
+        String name = request.getParameter("name");
+
+        
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String jdbcUrl = "jdbc:mysql://aagurobfnidxze.cesazkri7ef1.us-east-2.rds.amazonaws.com:3306/game?user=user&password=password";    
+        String returnVal = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(jdbcUrl);
+            //if (start.trim() != null) {
+                //insert and find the player’s id
+                //ps = conn.prepareStatement("INSERT INTO Threads (username) VALUES (‘"+ name+ "’);");
+                //rs = ps.executeQuery();
+                //System.out.println("here”);
+                //find out the id
+//                ps = conn.prepareStatement("SELECT COUNT (username) FROM Threads”);
+//                rs = ps.executeQuery();
+                //while(rs.next()) {    //if found any
+                //    returnVal = "1”;
+                //}
+
+            //.conn.}
+            //else {
+                ps = conn.prepareStatement("SELECT * FROM Threads WHERE threadID=?");
+    
+                ps.setString(1, currentID+1);
+                rs = ps.executeQuery();
+    
+                while(rs.next()) {    //if found any
+                    returnVal = rs.getString("username").trim();
+                }
+
+        }catch(SQLException sqle) {
+        }catch(ClassNotFoundException cnfe) {
+        }finally {
+            try {
+                if(rs != null) rs.close();
+                if(ps != null) ps.close();
+                if(conn != null) conn.close();
+            }catch (SQLException sqle) {
+                System.out.println("sqle: " + sqle.getMessage());
+            }
+        }
+
+        response.setContentType("text/html");
+        out.print(returnVal);
+        out.flush();
+        out.close();
+        
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
